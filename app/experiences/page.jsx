ad22211a64dfafd1fccbb174a9126342d47b548d
Phạm Anh Dũng <168076412+PhamAnhDungdev@users.noreply.github.com>
2024-09-27 12:05:02 +0700

    #Update experience page and projects/others

--- app/experiences/page.jsx
+++ app/experiences/page.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 const Experiences = ({ hidden }) => {
   return (
-    <div className={`flex ${hidden ? 'hidden' : 'block'}`}>
-      Experiences
-    </div>
-  )
-}
+    <div className={`flex flex-col justify-center items-center ${hidden ? "hidden" : "block"}`}>
 
-export default Experiences
+
+      <div className="flex md:w-[750px] max-sm:w-[550px] max-sm:ml-40 bg-gray-200 my-10 h-32 rounded-lg items-center">
+        <Image
+          src="/skillsfw/js.png"
+          width={110}
+          height={110}
+          alt="Skill JavaScript"
+          className="ml-16 border-4 border-gray-100"
+        />
+        <div className="flex flex-col ml-10 space-y-2">
+          <h1 className="font-bold text-3xl text-orange-500">JavaScript</h1>
+          <div className="rounded-full border-2 w-36 border-gray-950 bg-white">
+            <h1 className="text-center font-bold"> 4 Month </h1>  
+          </div>
+          <p className="font-sans text-lg">4 month experiences with JavaScript</p>
+        </div>
+      </div>
+
+      <div className="flex w-[750px] bg-gray-200 my-10 h-32 rounded-lg items-center">
+        <Image
+          src="/skillsfw/nextjs.png"
+          width={110}
+          height={110}
+          alt="Skill JavaScript"
+          className="ml-16 border-4 border-gray-100 bg-white"
+        />
+        <div className="flex flex-col ml-10 space-y-2">
+          <h1 className="font-bold text-3xl text-orange-500">NextJs</h1>
+          <div className="rounded-full border-2 w-36 border-gray-950 bg-white">
+            <h1 className="text-center font-bold"> 4 Month </h1>  
+          </div>
+          <p className="font-sans text-lg">4 month experiences with NextJs.</p>
+        </div>
+      </div>
+
+
+      <div className="flex w-[750px] bg-gray-200 my-10 h-32 rounded-lg items-center">
+        <Image
+          src="/skillsfw/java.png"
+          width={110}
+          height={110}
+          alt="Skill JavaScript"
+          className="ml-16 border-4 border-gray-100 bg-white"
+        />
+        <div className="flex flex-col ml-10 space-y-2">
+          <h1 className="font-bold text-3xl text-orange-500">Java</h1>
+          <div className="rounded-full border-2 w-36 border-gray-950 bg-white">
+            <h1 className="text-center font-bold"> 1 Year </h1>  
+          </div>
+          <p className="font-sans text-lg">1 year experiences with Java</p>
+        </div>
+      </div>
+
+
+      <div className="flex w-[750px] bg-gray-200 my-10 h-32 rounded-lg items-center">
+        <Image
+          src="/skillsfw/csharp.png"
+          width={110}
+          height={110}
+          alt="Skill JavaScript"
+          className="ml-16 border-4 border-gray-100 bg-white"
+        />
+        <div className="flex flex-col ml-10 space-y-2">
+          <h1 className="font-bold text-3xl text-orange-500">C# .NET</h1>
+          <div className="rounded-full border-2 w-36 border-gray-950 bg-white">
+            <h1 className="font-bold text-center"> 6 Month </h1>  
+          </div>
+          <p className="font-sans text-lg">6 month experiences with C#, .NET.</p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Experiences;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 className='font-semibold text-3xl mb-5'>Updating...</h1>
+      <Oval height={40} width={40} />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
